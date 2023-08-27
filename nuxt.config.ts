d63795b58d20c2fe87fbb1a69c979ab829d4902f
Nicolas <walcker_@outlook.com>
2023-08-27 09:48:45 -0300

    feat: adds nuxt-icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/eslint-module',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    'nuxt-icon'
   ],
   tailwindcss: {
     cssPath: '~/assets/css/global.css',
