2a8eb66aae9974079a18ed6f15ec92942e4d466c
Nicolas Walcker <walcker_@outlook.com>
2023-08-30 18:40:41 -0300

    refactor: update select component

--- components/SelectComponent.vue
+++ components/SelectComponent.vue
@@ -14,8 +14,8 @@
         @click="toggleMenu"
       >
         <span class="flex items-center">
-          <Icon :name="`ri:${selected}-fill`" />
-          <span class="ml-3 block truncate first-letter:capitalize">{{ selected }}</span>
+          <Icon :name="selected.icon" />
+          <span class="ml-3 block truncate first-letter:capitalize">{{ getSocial(selected.name) }}</span>
         </span>
         <span
           class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"
@@ -25,32 +25,23 @@
       </button>
       <ul
         v-if="menu"
-        class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-base-100 py-1 text-base shadow-lg ring-1 ring-base-content ring-opacity-5 focus:outline-none sm:text-sm"
+        class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-base-100 py-1 text-base shadow-lg ring-1 ring-base-content/5 focus:outline-none sm:text-sm"
         tabindex="-1"
         role="listbox"
         aria-labelledby="listbox-label"
         aria-activedescendant="listbox-option-3"
       >
         <li
-          id="listbox-option-0"
-          class="text-base-content relative cursor-pointer select-none py-2 pl-3 pr-9 hover:text-primary"
+          v-for="(item, index) in socialList"
+          :id="`listbox-option-${index}`"
+          :key="index"
+          class="relative cursor-pointer select-none py-2 pl-3 pr-9 text-base-content hover:text-primary"
           role="option"
-          @click="onSelect('facebook')"
+          @click="onSelect(item)"
         >
           <div class="flex items-center">
-            <Icon name="ri:facebook-fill" size="24" />
-            <span class="font-normal ml-3 block truncate">Facebook</span>
-          </div>
-        </li>
-        <li
-          id="listbox-option-0"
-          class="text-base-content relative cursor-pointer select-none py-2 pl-3 pr-9 hover:text-primary"
-          role="option"
-          @click="onSelect('twitter')"
-        >
-          <div class="flex items-center">
-            <Icon name="ri:twitter-fill" size="24" />
-            <span class="font-normal ml-3 block truncate">Twitter</span>
+            <Icon :name="item.icon" size="24" />
+            <span class="ml-3 block truncate font-normal first-line:capitalize">{{ getSocial(item.name) }}</span>
           </div>
         </li>
       </ul>
@@ -59,23 +50,94 @@
 </template>
 
 <script lang="ts" setup>
+import { Social } from '@/utils/types/social'
 const menu = ref(false)
 const toggleMenu = () => {
   menu.value = !menu.value
 }
 
-const prop = defineProps({
-  selected: {
-    type: String,
-    default: 'Facebook'
-  }
+const prop = withDefaults(defineProps<{
+  selected: Social
+}>(), {
+  selected: () => ({
+    name: 'facebook',
+    icon: 'cib:facebook-f'
+  })
 })
 
-const selected = ref(prop.selected)
+const selected = ref<Social>(prop.selected)
 
 const emit = defineEmits(['update:selected'])
 
-const onSelect = (value: string) => {
+// todo: adds slug item on social object
+const socialList = ref<Array<Social>>([
+  {
+    name: 'facebook',
+    icon: 'cib:facebook-f'
+  },
+  {
+    name: 'twitter',
+    icon: 'cib:twitter'
+  },
+  {
+    name: 'instagram',
+    icon: 'cib:instagram'
+  },
+  {
+    name: 'linkedin',
+    icon: 'cib:linkedin-in'
+  },
+  {
+    name: 'github',
+    icon: 'cib:github'
+  },
+  {
+    name: 'youtube',
+    icon: 'cib:youtube'
+  },
+  {
+    name: 'twitch',
+    icon: 'cib:twitch'
+  },
+  {
+    name: 'tiktok',
+    icon: 'cib:tiktok'
+  },
+  {
+    name: 'spotify',
+    icon: 'cib:spotify'
+  },
+  {
+    name: 'pinterest',
+    icon: 'cib:pinterest-p'
+  },
+  {
+    name: 'behance',
+    icon: 'cib:behance'
+  },
+  {
+    name: 'dribbble',
+    icon: 'cib:dribbble'
+  },
+  {
+    name: 'medium',
+    icon: 'cib:medium-m'
+  },
+  {
+    name: 'reddit',
+    icon: 'cib:reddit-alt'
+  },
+  {
+    name: 'dev',
+    icon: 'cib:dev-to'
+  }
+])
+
+const getSocial = (name: string) => {
+  return name.includes('-') ? name.split('-').join(' ') : name
+}
+
+const onSelect = (value: Social) => {
   selected.value = value
   emit('update:selected', value)
   toggleMenu()
