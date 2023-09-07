d8e586500693113df7699b251899eef7dde31166
Nicolas Walcker <walcker_@outlook.com>
2023-09-07 14:45:54 -0300

    fear: create error item

---
+++ components/ErrorItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <span v-if="error" class="text-sm font-medium text-error">
+    {{ error }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+withDefaults(defineProps<{
+  error: string
+}>(), {
+  error: ''
+})
+</script>
