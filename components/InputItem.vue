c8ad06c23986e1cdb8f59d9b3327ae1d1f8eddf7
Nicolas Walcker <walcker_@outlook.com>
2023-09-07 14:46:10 -0300

    feat: create new input item

---
+++ components/InputItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form-control w-full justify-between gap-1" :class="isRow ? 'md:flex-row md:items-center' : ''">
+    <label class="label" :for="name">{{ label }}</label>
+    <input
+      :id="name"
+      :name="name"
+      :placeholder="placeholder"
+      :type="type"
+      class="input input-bordered input-ghost"
+      :class="isRow ? 'w-full md:w-[65%]' : ''"
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+withDefaults(defineProps<{
+  label: string,
+  type?: 'password' | 'email' | 'text',
+  modelValue: string,
+  placeholder: string,
+  name: string,
+  isRow?: boolean,
+}>(), {
+  type: 'text',
+  isRow: false,
+  modelValue: ''
+})
+
+defineEmits(['update:modelValue'])
+</script>
