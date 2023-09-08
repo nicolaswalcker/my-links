3e6ed8daaeebec5ea30a11a6367836f216b00ab1
Nicolas <walcker_@outlook.com>
2023-09-08 13:26:53 -0300

    feat: update layout and create logout function

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,24 +8,8 @@
     </div>
 
     <nav>
-      <ul class="flex items-center gap-3">
-        <li>
-          <nuxt-link :class="smallerThanSm ? 'btn-square' : ''" class="btn" active-class="btn-neutral" to="/links">
-            <Icon name="solar:link-circle-bold" size="24" />
-            <p class="hidden sm:block">
-              Links
-            </p>
-          </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link :class="smallerThanSm ? 'btn-square' : ''" class="btn" active-class="btn-neutral" to="/profile">
-            <Icon name="solar:user-circle-bold" size="24" />
-            <p class="hidden sm:block">
-              Perfil
-            </p>
-          </nuxt-link>
-        </li>
-        <li>
+      <ul class="flex items-center gap-3" role="menu">
+        <li role="menuitem">
           <nuxt-link :class="smallerThanSm ? 'btn-square' : ''" class="btn" active-class="btn-neutral" to="/">
             <Icon name="solar:gallery-bold" size="24" />
             <p class="hidden sm:block">
@@ -33,9 +17,25 @@
             </p>
           </nuxt-link>
         </li>
+        <li role="menuitem">
+          <nuxt-link :class="smallerThanSm ? 'btn-square' : ''" class="btn" active-class="btn-neutral" to="/links">
+            <Icon name="solar:link-circle-bold" size="24" />
+            <p class="hidden sm:block">
+              Links
+            </p>
+          </nuxt-link>
+        </li>
+        <li role="menuitem">
+          <nuxt-link :class="smallerThanSm ? 'btn-square' : ''" class="btn" active-class="btn-neutral" to="/profile">
+            <Icon name="solar:user-circle-bold" size="24" />
+            <p class="hidden sm:block">
+              Perfil
+            </p>
+          </nuxt-link>
+        </li>
       </ul>
     </nav>
-    <button :class="smallerThanSm ? 'btn-square' : ''" class="btn btn-outline">
+    <button :class="smallerThanSm ? 'btn-square' : ''" class="btn btn-outline" @click="logout">
       <Icon name="solar:logout-3-bold" size="18" />
       <p class="hidden sm:block">
         Sair
@@ -53,4 +53,12 @@ import { breakpointsTailwind } from '@vueuse/core'
 const breakpoints = useBreakpoints(breakpointsTailwind)
 
 const smallerThanSm = breakpoints.smaller('sm')
+
+const supabase = useSupabaseClient()
+const router = useRouter()
+
+const logout = async () => {
+  await supabase.auth.signOut()
+  router.push('/login')
+}
 </script>
